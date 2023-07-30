a64d35c5e698b6518722a66fd6d8406e50fae1ce
coderiderlin <coderiderlin@gmail.com>
2023-07-31 04:40:29 +0800

    string hashcode ok,but trace to file still truncated.

--- unidbg-android/src/test/java/re/qqsdk/Fekit.java
+++ unidbg-android/src/test/java/re/qqsdk/Fekit.java
@@ -5,10 +5,8 @@ import com.github.unidbg.Module;
 import com.github.unidbg.arm.backend.Unicorn2Factory;
 import com.github.unidbg.linux.android.AndroidEmulatorBuilder;
 import com.github.unidbg.linux.android.AndroidResolver;
-import com.github.unidbg.linux.android.dvm.AbstractJni;
-import com.github.unidbg.linux.android.dvm.Array;
-import com.github.unidbg.linux.android.dvm.DalvikModule;
-import com.github.unidbg.linux.android.dvm.VM;
+import com.github.unidbg.linux.android.dvm.*;
+import com.github.unidbg.linux.android.dvm.wrapper.DvmInteger;
 import com.github.unidbg.memory.Memory;
 import com.github.unidbg.utils.Inspector;
 import re.util.LogUtil;
@@ -18,6 +16,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
 import java.util.Arrays;
 import java.util.List;
 
@@ -74,13 +73,51 @@ public class Fekit extends AbstractJni {
 
         int func_start = 0x62880;
         int func_end = 0x75074;
-//        emulator.traceCode();
 
 
 //            emulator.traceCode(dmFekit.getModule().base+func_start,dmFekit.getModule().base+func_end)
 //            emulator.traceCode(dmFekit.getModule().base,dmFekit.getModule().base+dmFekit.getModule().size)
         emulator.traceCode()
                 .setRedirect(traceStream);
+
+//        emulator.traceCode();
+
+    }
+
+    @Override
+    public DvmObject<?> callStaticObjectMethod(BaseVM vm, DvmClass dvmClass, String signature, VarArg varArg) {
+
+        LogUtil.i("callStaticObjectMethod",String.format("%s %s",signature,varArg));
+        switch (signature) {
+//            case "android/os/SystemProperties->get(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;":
+//                System.out.println("android/os/SystemProperties->get " + varArg);
+//                return new StringObject(vm, "705KPGS001091");
+//            case "android/app/ActivityThread-&gt;currentActivityThread()Landroid/app/ActivityThread;":
+//                return vm.resolveClass("android/app/ActivityThread").newObject(null);
+        }
+        return super.callStaticObjectMethod(vm, dvmClass, signature, varArg);
+    }
+
+    @Override
+    public int callIntMethodV(BaseVM vm, DvmObject<?> dvmObject, String signature, VaList vaList) {
+
+        String dvmObjectStr=dvmObject.toString();
+        if(dvmObject.getValue() instanceof String)
+        {
+            dvmObjectStr=(String)dvmObject.getValue();
+        }
+        LogUtil.i("callIntMethod",String.format("%s %s %s",signature,dvmObjectStr,vaList.toString()));
+
+        switch (signature) {
+            case "java/lang/String->hashCode()I": {
+                String str = (String) dvmObject.getValue();
+                int res= str.hashCode();
+                LogUtil.i(TAG,String.format("hashcode of %s is %d",str,res));
+                return res;
+            }
+
+        }
+        return super.callIntMethodV(vm, dvmObject, signature, vaList);
     }
 
     public void callJNI_OnLoad()
@@ -94,10 +131,19 @@ public class Fekit extends AbstractJni {
         traceStream.flush();
     }
 
+    static void waitCtrlC()
+    {
+        LogUtil.i(TAG,String.format("pid:%s,exec done.press ctrl-c to exit.", ManagementFactory.getRuntimeMXBean().getName().split("@")[0]));
+        while(true)
+        {
+            Utils.sleep(3000);
+        }
+    }
     public static void main(String[] args) {
         try {
             Fekit fekit = new Fekit();
             fekit.callJNI_OnLoad();
+//            waitCtrlC();
         } catch (Exception ex) {
             LogUtil.e(TAG, "error on main:"+LogUtil.getStackTraceString(ex));
         }
